86697a5efa9758d9a5d9c274d2dc1ccf135f8ee7
HyunSun Ahn <kiwibird0417@gmail.com>
2024-12-04 18:23:50 +0900

    Initial Commit

---
+++ Project2_1204/Project5_1.cpp
@@ -0,0 +1,33 @@
+//5-1. 조건문
+/*
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	//1)분기구문/조건문 : if문
+	if (false) {
+		cout << "조건이";
+		cout << "참입니다." << endl;
+	}
+	else {
+		cout << "조건이";
+		cout << " 거짓입니다." << endl;
+	}
+
+	cout << "프로그램이 종료되었습니다.";
+	//======================================================
+	//예시2
+	if (false) {
+		cout << "#1";
+	}
+	else if (true) {
+		cout << "#2";
+	}
+	else {
+		cout << "#1";
+	}
+
+	return 0;
+}
+*/
---
+++ Project2_1204/Project5_3.cpp
@@ -0,0 +1,67 @@
+//5-3. switch문과 break continue문
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	//1)switch문
+	/*
+	int a;
+	cin >> a;
+
+	switch (a) {
+	case 1:
+		cout << "입력하신 값은 1입니다.\n";
+		break;
+	case 2:
+		cout << "입력하신 값은 2입니다.\n";
+		break;
+	case 3:
+		cout << "입력하신 값은 3입니다.\n";
+		break;
+	default:
+		cout << "입력하신 값은 1,2,3 이외의 값입니다.\n";
+		break;	//break가 없으면, default 값도 그대로 출력이 되는 것이다.
+		//모든 반복문에서 break 사용 가능
+	}
+
+	cout << "switch문이 끝났습니다.\n";
+	*/
+
+	//=====================================================
+	//2) while문에서 break를 이용한 탈출
+	int i = 0;
+	while (true) {
+		cout << "i의 값은 " << i << "입니다." << endl;
+		if (i > 10) {
+			break;
+		}
+		i++;
+	}
+
+	cout << "while문이 끝났습니다.\n\n";
+
+	//==============================================
+	//3)continue문
+	const int SIZE = 30;
+
+	cout << "문장을 입력하십시오.\n";
+	char line[SIZE];
+	cin.get(line, SIZE);
+
+	cout << "입력하신 문장은\n";
+	int spaces = 0;
+	for (int i = 0;line[i] != '\0'; i++) {
+		cout << line[i];
+
+		if (line[i] != ' ') {
+			continue;
+		}
+		spaces++;
+	}
+	cout << "입니다.\n";
+	cout << "입력하신 문장에서 공백을 제외한 문자 수는 " << spaces << "개입니다.\n";
+
+
+	return 0;
+}
